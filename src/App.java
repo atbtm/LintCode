c6e651ae88cb2567f8738dd61471326594e91389
atbtm <atbtmmm@gmail.com>
2018-07-02 01:04:32 -0400

    uptil K closest numbers Q:"Find K Closest Elements"

--- src/App.java
+++ src/App.java
@@ -1,6 +1,22 @@
 
 public class App {
-	public App(){}
+	public int longestPalindrome(String s) {
+		if (s == null || s.length() == 0){
+			return 0;
+		}
+		int[] lut = new int['z' - 'A' + 1];
+		for (char c: s.toCharArray()){
+			lut[c - 'A']++;
+		}
+		boolean isOdd = false;
+		int res = 0;
+		for (int cnt: lut){
+			res += cnt / 2 * 2;
+			isOdd = cnt % 2 == 1 ? true : isOdd;
+		}
+		return res + (isOdd ? 1 : 0);
+	}
+
 	public boolean isPalindrome(String s) {
 		if (s == null){
 			return false;
@@ -9,30 +25,175 @@ public class App {
 			return true;
 		}
 		char[] charArr = s.toCharArray();
-        for (int left = 0, right = s.length()-1; left < right;){
-        	if (!Character.isDigit(charArr[left]) && !Character.isLetter(charArr[left])){
-        		left ++;
-        		continue;
-        	}
-        	if (!Character.isDigit(charArr[right]) && !Character.isLetter(charArr[right])){
-        		right --;
-        		continue;
-        	}
-        	if (Character.toLowerCase(charArr[left]) != Character.toLowerCase(charArr[right])){
-        		return false;
-        	}
-        	left++;
-        	right--;
-        }
+		for (int left = 0, right = s.length()-1; left < right;){
+			if (!Character.isDigit(charArr[left]) && !Character.isLetter(charArr[left])){
+				left ++;
+				continue;
+			}
+			if (!Character.isDigit(charArr[right]) && !Character.isLetter(charArr[right])){
+				right --;
+				continue;
+			}
+			if (Character.toLowerCase(charArr[left]) != Character.toLowerCase(charArr[right])){
+				return false;
+			}
+			left++;
+			right--;
+		}
 		return true;
+	}
+
+	/*
+	 * @param source: source string to be scanned.
+	 * @param target: target string containing the sequence of characters to match
+	 * @return: a index to the first occurrence of target in source, or -1  if target is not part of source.
+	 */
+	public int strStr(String source, String target) {
+		if (source == null || target == null) {
+			return -1;
+		}
+		if (source.length() < target.length()) {
+			return -1;
+		}
+		if (target.length() == 0) {
+			return 0;
+		}
+		for (int i = 0; i <= source.length() - target.length(); i++) {
+			int j = 0;
+			for (j = 0; j < target.length(); j++) {
+				if (source.charAt(i+j) != target.charAt(j)) {
+					break;
+				}
+			}
+			if (j == target.length()) { 
+				return i;
+			}
+		}
+		return -1;
+	}
+	
+	public String longestPalindromeString(String s) {
+		if (s == null) {
+			return null;
+		}
+		int startIdx = 0;
+		int longestLen = 0;
+		char[] charArr = s.toCharArray();
+		for (int symAxis = 0; symAxis < s.length(); symAxis++) {
+			int offset = 0;
+			while (symAxis - offset >= 0 && symAxis + offset < s.length()
+				&& charArr[symAxis - offset] == charArr[symAxis + offset]) {
+				int len = offset * 2 + 1;
+				if (len > longestLen) {
+					longestLen = len;
+					startIdx = symAxis - offset;
+				}
+				offset++;
+			}
+			offset = 0;
+			while (symAxis - offset >= 0 && symAxis + offset + 1 < s.length()
+				&& charArr[symAxis - offset] == charArr[symAxis + offset + 1]) {
+				int len = offset * 2 + 2;
+				if (len > longestLen) {
+					longestLen = len;
+					startIdx = symAxis - offset;
+				}
+				offset++;
+			}
+		}
+		return s.substring(startIdx, startIdx + longestLen);
     }
 	
-	/*
-     * @param source: source string to be scanned.
-     * @param target: target string containing the sequence of characters to match
-     * @return: a index to the first occurrence of target in source, or -1  if target is not part of source.
+	public int binarySearch(int[] nums, int target) {
+		if (nums == null) {
+			return -1;
+		}
+		int left = 0;
+		int right = nums.length - 1;
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[mid] > target) {
+				right = mid - 1;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return -1;
+	}
+	
+    public int lastPosition(int[] nums, int target) {
+    	if (nums == null || nums.length == 0) {
+    		return -1;
+    	}
+    	int left = 0;
+    	int right = nums.length - 1;
+    	while (left + 1 < right) {
+    		int mid = left + (right - left) / 2;
+    		if (nums[mid] <= target) {
+    			left = mid;
+    		} else {
+    			right = mid - 1;
+    		}
+    	}
+    	return nums[right] == target ? right : nums[left] == target ? left : -1;
+    }
+    
+    /**
+     * @param nums: a mountain sequence which increase firstly and then decrease
+     * @return: then mountain top
      */
-    public int strStr(String source, String target) {
-       return 0;
+    public int mountainSequence(int[] nums) {
+    	if (nums == null || nums.length == 0) {
+    		return -1;
+    	}
+    	if (nums.length == 1) {
+    		return nums[0];
+    	}
+    	int left = 0;
+    	int right = nums.length - 1;
+    	while (left + 1 < right) {
+    		int mid = left + (right - left) / 2;
+    		if (nums[mid] > nums[mid + 1]) {
+    			right = mid;
+    		} else if (nums[mid] < nums[mid + 1]) {
+    			left = mid + 1;
+    		} else {
+    			return nums[mid];
+    		}
+    	}
+    	return Math.max(nums[left], nums[right]);
+    }
+    
+    /**
+     * @param A: an integer array
+     * @param target: An integer
+     * @param k: An integer
+     * @return: an integer array
+     * 
+     * Given A = [1, 2, 3], target = 2 and k = 3, return [2, 1, 3].
+	 * Given A = [1, 4, 6, 8], target = 3 and k = 3, return [4, 1, 6].
+     */
+    public int[] kClosestNumbers(int[] A, int target, int k) {
+        if (A == null || A.length == 0) {
+        	return null;
+        }
+        int left = 0;
+        int right = A.length - 1;
+        int targetIdx = -1;
+        while (left < right) {
+        	int mid = left + (right - left) / 2;
+        	if (A[mid] < target) {
+        		left = mid + 1;
+        	}else if(A[mid] > target){
+        		right = mid - 1;
+        	}else {
+                left = mid;
+                right = mid + 1;
+        	}
+        }
+        int [] res = new int[k];
+        
     }
 }
--- src/AppTest.java
+++ src/AppTest.java
@@ -1,10 +1,24 @@
 
 import org.junit.Assert;
-import org.junit.BeforeClass;
+import org.junit.Before;
 import org.junit.Test;
 
 public class AppTest {
-	private static App 	app = new App();
+	private static App app ;
+	
+	@Before
+	public void setup() {
+		app = new App();
+	}
+	
+	@Test(timeout=1000)
+	public void testLongestPalindrome() {
+		Assert.assertEquals(app.longestPalindrome(null), 0);
+		Assert.assertEquals(app.longestPalindrome(""), 0);
+		Assert.assertEquals(app.longestPalindrome("a"), 1);
+		Assert.assertEquals(app.longestPalindrome("ab"), 1);
+		Assert.assertEquals(app.longestPalindrome("abbbbb"), 5);
+	}
 	
 	@Test(timeout=1000)
 	public void testIsPalindrome() {
@@ -21,9 +35,56 @@ public class AppTest {
 			Assert.assertFalse(app.isPalindrome(in));
 		}
 	}
-	
-	@Test
+
+	@Test(timeout=1000)
 	public void testStrStr(){
-		Assert.assertEquals(app.strStr("aaaabc", "ab"), 0);
+		Assert.assertEquals(app.strStr(null, "ab"), -1);
+		Assert.assertEquals(app.strStr("", "ab"), -1);
+		Assert.assertEquals(app.strStr("aaabc", "ab"), 2);
+		Assert.assertEquals(app.strStr("abababc", "ababc"), 2);
+		Assert.assertEquals(app.strStr("source", "rced"), -1);
+		Assert.assertEquals(app.strStr("", ""), 0);
+	}
+	
+	@Test(timeout=1000)
+	public void testLongestPalindromeString() {
+		Assert.assertEquals(null, app.longestPalindromeString(null));
+		Assert.assertEquals("", app.longestPalindromeString(""));
+		Assert.assertEquals("a", app.longestPalindromeString("abc"));
+		Assert.assertEquals("a", app.longestPalindromeString("a"));
+		Assert.assertEquals("aba", app.longestPalindromeString("aba"));
+		Assert.assertEquals("bab", app.longestPalindromeString("baba"));
+		Assert.assertEquals("3bdffdb3", app.longestPalindromeString("c3bdffdb3aaaa"));
+	}
+	
+	@Test(timeout = 1000)
+	public void testBinarySearch() {
+		Assert.assertEquals(-1, app.binarySearch(null, 0));
+		Assert.assertEquals(-1, app.binarySearch(new int[]{}, 0));
+		Assert.assertEquals(0, app.binarySearch(new int[]{0}, 0));
+		Assert.assertEquals(1, app.binarySearch(new int[]{0, 1}, 1));
+	}
+
+	@Test(timeout = 1000)
+	public void testLastPosition() {
+		Assert.assertEquals(-1, app.lastPosition(null, 0));
+		Assert.assertEquals(-1, app.lastPosition(new int[]{}, 0));
+		Assert.assertEquals(0, app.lastPosition(new int[]{0}, 0));
+		Assert.assertEquals(1, app.lastPosition(new int[]{0, 1}, 1));
+		Assert.assertEquals(2, app.lastPosition(new int[]{0, 1, 1}, 1));
+		Assert.assertEquals(3, app.lastPosition(new int[]{0, 1, 1, 1, 2}, 1));
+	}
+
+	@Test(timeout = 1000)
+	public void testmountainSequence() {
+		Assert.assertEquals(-1, app.mountainSequence(null));
+		Assert.assertEquals(-1, app.mountainSequence(new int[] {}));
+		Assert.assertEquals(2, app.mountainSequence(new int[] {1, 2, 1}));
+		Assert.assertEquals(2, app.mountainSequence(new int[] {1, 2, 1}));
+		Assert.assertEquals(2, app.mountainSequence(new int[] {0, 1, 2, 1}));
+		Assert.assertEquals(2, app.mountainSequence(new int[] {2, 1}));
+		Assert.assertEquals(12, app.mountainSequence(new int[] {1, 12}));
+		Assert.assertEquals(12, app.mountainSequence(new int[] {12, 1}));
+		Assert.assertEquals(12, app.mountainSequence(new int[] {1, 2, 3, 12, 3, 1}));
 	}
 }
